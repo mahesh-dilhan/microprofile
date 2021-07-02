1c191811f1926fa236cd11fa1b824d8a682748b8
mahesh <mail.mahesh.lk@gmail.com>
2021-07-02 17:40:50 +0800

    Microprofile

---
+++ welcome/service-a/src/main/java/io/mahesh/microprofile/welcome/WelcomeRestApplication.java
@@ -0,0 +1,11 @@
+package io.mahesh.microprofile.welcome;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class WelcomeRestApplication extends Application {
+}
---
+++ welcome/service-b/src/main/java/io/mahesh/microprofile/welcome/WelcomeRestApplication.java
@@ -0,0 +1,12 @@
+package io.mahesh.microprofile.welcome;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+
+public class WelcomeRestApplication extends Application {
+}
